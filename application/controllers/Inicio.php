4667cc0369adff309d5e5dd070bdb25edafd48d4
Eduardo Zamora <eazamo25@gmail.com>
2018-10-06 18:44:12 -0300

    descomentada funcion is_logged_in()

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Inicio extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
-		// is_logged_in();
+		is_logged_in();
 	}
 	public function index()
 	{
