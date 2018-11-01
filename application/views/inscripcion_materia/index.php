110871c15be70adae73739f263a97893627d6720
oaelias <oaelias@copade1413.copade.neuquen.gov.ar>
2018-11-01 11:19:26 -0300

    Abm Materia

---
+++ application/views/inscripcion_materia/index.php
@@ -0,0 +1,61 @@
+<div class="row">
+	<div class="col-12">
+
+		<div class="card">
+			<div class="card-body">
+				<!-- <h5 class="card-title">Basic Datatable</h5> -->
+				<div class="table-responsive">
+					<div id="zero_config_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
+						<div class="row">
+							<div class="col-sm-12">
+								<table id="zero_config" class="table table-striped table-bordered dataTable" role="grid" aria-describedby="zero_config_info">
+									<thead>
+										<tr role="row">
+
+											<th class="sorting_asc" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending">materia</th>
+											<th class="sorting_asc" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending">Alumno</th>
+											<th class="sorting_asc" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending">Acciones</th>
+										</tr>
+									</thead>
+
+									<tbody>
+										<?php foreach($inscripcion_materia as $i){ ?>
+											<tr>
+												<td><?php echo $i['id_materia'].' - '.$i['nombre_materia']; ?></td>
+                        <td><?php echo $i['nombre_persona'].' '.$i['apellido_persona'].', '.$i['tipo_documento_nombre'].' - '.$i['numero_documento']; ?></td>
+												<td>
+
+													<?php if($boton_edit){?>
+														<a href="<?php echo site_url('inscripcion_materia/edit/'.$i['id_persona'].'/'.$i['id_materia']); ?>" class="btn btn-info btn-sm">Editar</a>
+													<?php }?>
+													<?php if($boton_remove){?>
+														<a href="<?php echo site_url('inscripcion_materia/remove/'.$i['id_persona'].'/'.$i['id_materia']); ?>" data-confirm="remove" class="btn btn-danger btn-sm">Eliminar</a>
+													<?php }?>
+
+												</td>
+											</tr>
+										<?php } ?>
+									</tbody>
+
+									<tfoot>
+										<tr>
+											<th rowspan="1" colspan="1">materia</th>
+											<th rowspan="1" colspan="1">Alumno</th>
+											<th rowspan="1" colspan="1">Acciones</th>
+										</tr>
+									</tfoot>
+								</table>
+
+								<div class="pull-right">
+									<?php if ($boton_add): ?>
+										<a href="<?php echo site_url('inscripcion_materia/add'); ?>" class="btn btn-success">Nueva</a>
+									<?php endif; ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
