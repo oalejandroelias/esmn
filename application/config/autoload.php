2c13ce4000026a40607845fd1b5fb7f014d98f06
Alejandro <Alejandro@DESKTOP-3QR9SSG>
2018-11-01 15:48:55 -0300

    Modelo global y helper de fechas

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','form','login','globals');
+$autoload['helper'] = array('url','form','login','globals','fechas');
 
 /*
 | -------------------------------------------------------------------
