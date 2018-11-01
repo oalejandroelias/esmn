294a59a51bf35e914d47d4a75c99e6b71be1c963
Eduardo Zamora <eazamo25@gmail.com>
2018-11-01 12:05:43 -0300

    helper date.php

---
+++ application/helpers/date.php
@@ -0,0 +1,31 @@
+<?php
+//ejemplo de ecuacion para calcular cantidad de dias en un periodo
+// para usar en construccion de json "diascursado" en tabla curso
+
+$fecha_inicio = new DateTime('2018-03-05');
+$fecha_fin = new DateTime('2018-06-29');
+
+$dias_semana = array('martes','jueves'); //2
+
+$interval = $fecha_inicio->diff($fecha_fin);
+$dias_total = $interval->days; //116
+
+// print_r($interval);
+
+function countDaysByName($dayName, $start, $end)
+{
+    $count = 0;
+    $interval = new \DateInterval('P1D');
+    $period = new \DatePeriod($start, $interval, $end);
+
+    foreach($period as $day){
+        if($day->format('D') === ucfirst(substr($dayName, 0, 3))){
+            $count ++;
+        }
+    }
+    return $count;
+}
+echo countDaysByName('Tue',$fecha_inicio,$fecha_fin)." dias Martes y ";
+echo countDaysByName('Thu',$fecha_inicio,$fecha_fin)." dias Jueves";
+
+ ?>
