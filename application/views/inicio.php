7e4bd8c429f51a493dee04bb8e968d497a6e9c98
Eduardo Zamora <eazamo25@gmail.com>
2018-11-16 20:45:15 -0300

    cms embebido

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,19 +1,5 @@
-<div class="row">
-  <div class="col-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title"><?=$page_title?></h5>
-        <?php
-        echo "<pre>";
-        print_r($this->session->userdata());
-        print "Otro test de resolución de conflictos";
-        echo "</pre>";
-        ?>
-      </div>
-
-    </div>
-  </div>
-
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="http://localhost/wordpress" allowfullscreen></iframe>
 </div>
 
   <!-- Mapa -->
@@ -24,37 +10,37 @@
      <!-- Mapa -->
 
      <!-- Mapa -->
-  <div id="mapa"></div>
+  <!-- <div id="mapa"></div> -->
 
 
    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCOnpDL1OhCIE-a2oxHx2WVUTMNOhn5aSY&callback=initMap"></script>
     <script type="text/javascript">
-    function initialize() {
-      var marcadores = [
-
-        ['Escuela de musica', -38.952516, -68.059611], ['Escuela de musica', -37.952516, -66.059611]
-      ];
-      var map = new google.maps.Map(document.getElementById('mapa'), {
-        zoom: 14,
-        center: new google.maps.LatLng(-38.952516, -68.059611),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
-
-      var infowindow = new google.maps.InfoWindow();
-      var marker, i;
-      for (i = 0; i < marcadores.length; i++) {
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(marcadores[i][1], marcadores[i][2]),
-          map: map
-        });
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-          return function() {
-            infowindow.setContent(marcadores[i][0]);
-            infowindow.open(map, marker);
-          }
-        })(marker, i));
-      }
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
+    // function initialize() {
+    //   var marcadores = [
+    //
+    //     ['Escuela de musica', -38.952516, -68.059611], ['Escuela de musica', -37.952516, -66.059611]
+    //   ];
+    //   var map = new google.maps.Map(document.getElementById('mapa'), {
+    //     zoom: 14,
+    //     center: new google.maps.LatLng(-38.952516, -68.059611),
+    //     mapTypeId: google.maps.MapTypeId.ROADMAP
+    //   });
+    //
+    //   var infowindow = new google.maps.InfoWindow();
+    //   var marker, i;
+    //   for (i = 0; i < marcadores.length; i++) {
+    //     marker = new google.maps.Marker({
+    //       position: new google.maps.LatLng(marcadores[i][1], marcadores[i][2]),
+    //       map: map
+    //     });
+    //     google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    //       return function() {
+    //         infowindow.setContent(marcadores[i][0]);
+    //         infowindow.open(map, marker);
+    //       }
+    //     })(marker, i));
+    //   }
+    // }
+    // google.maps.event.addDomListener(window, 'load', initialize);
     </script>
  <!-- Mapa -->
